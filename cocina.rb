7d50cb46898fdf4b96b56548bea27bce7d1fa2fc
Adam Munoz Lopez <journeytoeternity@gmail.com>
2011-12-17 19:36:10 +0100

    todo manage config with gconf2

--- cocina.rb
+++ cocina.rb
@@ -1,8 +1,10 @@
 require 'gui/main'
+#todo Manage configuration settings with GCONF
+HOST = '192.168.1.11'
 
 begin
   @app = App.new
-  Thread.new {@app.start_network('192.168.1.11')}
+  Thread.new {@app.start_network(HOST)}
   @app.run
 ensure
   @app.clean
